4e0e1bac59ae69ef55d41e653f0ef616d840ced7
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-19 06:27:41 -0300

    Add POST "/productWithCategory"

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateCategoryController } from "./controllers/Categories/CreateCategoryController";
 import { GetCategoriesController } from "./controllers/Categories/GetCategoriesController";
 import { CreateProductController } from "./controllers/Products/CreateProductController";
+import { CreateProductWithCategoryController } from "./controllers/Products/CreateProductWithCategoryController";
 import { CreateProductCategoryController } from "./controllers/_Relations/CreateProductCategoryController";
 
 /* Get Controllers*/
@@ -9,6 +10,7 @@ const getCategories = new GetCategoriesController();
 
 /* Create Controllers*/
 const createProduct = new CreateProductController();
+const createProductWithCategory = new CreateProductWithCategoryController();
 const createCategory = new CreateCategoryController();
 const createProductCategory = new CreateProductCategoryController();
 
@@ -18,6 +20,7 @@ router.get("/categories", getCategories.handle)
 
 /* POST */
 router.post("/product", createProduct.handle)
+router.post("/productWithCategory", createProductWithCategory.handle)
 router.post("/category", createCategory.handle)
 router.post("/productCategory", createProductCategory.handle)
 
