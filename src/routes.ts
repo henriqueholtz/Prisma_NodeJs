8b916a61fc08f6bed6c3e1736762c4059ec92cf0
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-19 06:06:51 -0300

    Add POST "/productCategory" (relation from Product and Category: many to many)

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/Categories/CreateCategoryController";
 import { CreateProductController } from "./controllers/Products/CreateProductController";
+import { CreateProductCategoryController } from "./controllers/_Relations/CreateProductCategoryController";
 
 const createProduct = new CreateProductController();
 const createCategory = new CreateCategoryController();
+const createProductCategory = new CreateProductCategoryController();
 
 const router = Router();
 router.post("/product", createProduct.handle)
 router.post("/category", createCategory.handle)
+router.post("/productCategory", createProductCategory.handle)
 
 export { router };
